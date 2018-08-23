aa7228aa3e5a4ce95a4f81bc10944ab08ffc757c
Paweł <37835885+pawelmusial1995@users.noreply.github.com>
2018-08-23 13:46:43 +0200

    advanced

--- jQueryUI/js/script.js
+++ jQueryUI/js/script.js
@@ -4,7 +4,12 @@
 
   $(function () {
 
-      
+      $('.box').draggable();
+       
+      $('#box1').draggable({ scroll:true});
+      $('#box2').draggable({ axis: "x"});
+      $('#box3').draggable({ axis: "y"});
+      $('#box4').draggable({ contaiment: ".container"});
       
   });  
    
